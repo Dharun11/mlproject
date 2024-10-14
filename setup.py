b3f831833c699dd7edcbc104b034639312165e4f
dharun11 <dharunashok113@gmail.com>
2024-10-14 12:43:37 +0530

    setup and req addede

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot="-e ."    
+def get_requirements(file_pathh:str)-> List[str]:
+    
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+     
+    with open(file_pathh) as f:
+        requirements = f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    
+    if hypen_e_dot in requirements:
+        requirements.remove(hypen_e_dot)
+    return requirements
+        
+         
+         
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Dharun balaji",
+    author_email="dharunashok113@gmail.com",
+    packages=find_packages(),  # this will automatically find the packages that we are using in the project
+    install_requires=get_requirements('requirements.txt')
+    
+)
