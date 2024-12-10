c0d6bf79d2f39a79d58f092621a31e22fb1c9810
dharun11 <dharunashok113@gmail.com>
2024-12-10 12:29:28 +0530

    Modified the flask app

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,12 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"  # this is the log folder name nd log file name both are same
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)  # this will create a folder name "logs" in cwd here we declare the path 
+os.makedirs(logs_path,exist_ok=True)   #creating the logs folder
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)   # this is the entire path inside logs folder , it will have a folder name in LOG_FILE format 
+# inside it will create name 
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -14,7 +15,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-'''
+
 if __name__=="__main__":
     logging.info("This is a log message LOGGING HAS STARTED")
-'''
+
